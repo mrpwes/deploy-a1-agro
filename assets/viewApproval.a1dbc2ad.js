de1515043c888ec797a092ac96006ff785299fec
mrpwes <31194909+mrpwes@users.noreply.github.com>
2024-10-28 10:59:35 +0800

    Refactor CSS files and remove unnecessary styles

--- assets/viewApproval.a1dbc2ad.js
+++
@@ -1,1 +0,0 @@
-import{at as t}from"./index.4b3c3d71.js";const s=t("viewApproval",{state:()=>({rows:[]}),getters:{getArchivedApprovalList(r){return r.rows.filter(e=>e.is_archive===!0)},getUnarchivedApprovalList(r){return r.rows.filter(e=>e.is_archive===!1)}},actions:{}});export{s as u};
