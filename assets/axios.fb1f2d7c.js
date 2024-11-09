2f7b03cb5b8370c0702c2080c7023f1e6436d5f6
mrpwes <31194909+mrpwes@users.noreply.github.com>
2024-11-10 02:39:04 +0800

    Remove unused CSS files and add new components for improved layout and functionality

--- assets/axios.fb1f2d7c.js
+++
@@ -1,1 +0,0 @@
-import{b as i}from"./index.fa342762.js";import{a}from"./axios.0ed95acd.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
--- assets/viewApproval.1d9c9894.js
+++
@@ -1,1 +0,0 @@
-import{at as t}from"./index.c0f9d41d.js";const s=t("viewApproval",{state:()=>({rows:[]}),getters:{getArchivedApprovalList(r){return r.rows.filter(e=>e.is_archive===!0)},getUnarchivedApprovalList(r){return r.rows.filter(e=>e.is_archive===!1)}},actions:{}});export{s as u};
